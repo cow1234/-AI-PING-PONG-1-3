110e48fe27830be6c603887fadcfa405b0d0f376
cow1234 <79946779+cow1234@users.noreply.github.com>
2022-11-30 20:35:44 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -23,6 +23,15 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent("canvas");
+  video=createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+  poseNet=ml5.poseNet(video, modelLoaded);
+  
+}
+function modelLoaded(){
+  console.log('poseNet is ready')
 }
 
 
